eb72ccffff26be6fae3ac13c6d41174f5fe06757
Jaime Nickels <140667293+JNickels1@users.noreply.github.com>
2024-04-11 13:24:27 -0500

    trying bug fix

--- server/src/utils/auth.utils.js
+++ server/src/utils/auth.utils.js
@@ -11,29 +11,22 @@ export function comparPassword(enteredPassword, usersPassword){
 }
 
 export function signJwt(user) {
-
-    const tokenUser = {
-        sub: user._id, 
-        username: user.username,
+    if(!user || !user._id || !user.username){
+        throw new Error("Invalid user object");
     }
-    return jwt.sign(tokenUser, JWT_SECRET, { expiresIn: JWT_TOKENLIFE})
+    let token;
+    try{
+        const tokenUser = {
+            sub: user._id, 
+            username: user.username,
+        }
+        token= jwt.sign(tokenUser, JWT_SECRET,{
+            expiresIn: JWT_TOKENLIFE
+        });
+    }catch(err){
+        throw new Error("Error signing the token: " + err.message);
+    }
+    return token;
 }
 
 
-
-// if(!user || !user._id || !user.username){
-//     throw new Error("Invalid user object");
-// }
-// let token;
-// try{
-//     const tokenUser = {
-//         sub: user._id, 
-//         username: user.username,
-//     }
-//     token= jwt.sign(tokenUser, JWT_SECRET,{
-//         expiresIn: JWT_TOKENLIFE
-//     });
-// }catch(err){
-//     throw new Error("Error signing the token: " + err.message);
-// }
-// return token;
