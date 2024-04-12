4950a2bb389cd5a46bae8c9c63f9f705b6b6a620
Jesse Crowe <jesse.crowe@mykenzie.snhu.edu>
2024-04-12 12:03:21 -0600

    background images added

--- client/src/pages/SignUpPage.jsx
+++ client/src/pages/SignUpPage.jsx
@@ -69,7 +69,7 @@ function SignUpPage() {
   const handleInputChange = (e) => setFormData({ ...formData, [e.target.name]: e.target.value })
 
   const handleAvatarSelection = (avatar) => {
-    setFormData({ ...formData, selectedAvatar: avatar})
+    setFormData({ ...formData, selectedAvatar: avatar })
   }
 
   const avatarsList = [
@@ -87,220 +87,225 @@ function SignUpPage() {
     "/animalAvatars/tiger.png"
   ]
 
-  return <Container>
-    <h1>Create an Account</h1>
-    <Form className="auth-form" onSubmit={handleSubmit}>
-      <Row className="longInput">
-        <Col md={6}>
-          <Form.Group className="mb-3">
-            <Form.Label>Username</Form.Label>
-            <Form.Control
-              type="text"
-              name="userName"
-              value={formData.userName}
-              onChange={handleInputChange}
-              autoComplete="off"
-              required
-            />
-            <Form.Control.Feedback type="invalid">
-              {errors.userName}
-            </Form.Control.Feedback>
-          </Form.Group>
-        </Col>
-      </Row>
-      <Row>
-        <Col md={6}>
-          <Form.Group className="mb-3">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              type="password"
-              name="password"
-              value={formData.password}
-              onChange={handleInputChange}
-              autoComplete="off"
-              required
-            />
-            <Form.Control.Feedback type="invalid">
-              {errors.password}
-            </Form.Control.Feedback>
-          </Form.Group>
-        </Col>
-        <Col md={6}>
-          <Form.Group className="mb-3">
-            <Form.Label>Confirm Password</Form.Label>
-            <Form.Control
-              type="password"
-              name="confirmPassword"
-              value={formData.confirmPassword}
-              onChange={handleInputChange}
-              required
-            />
-            <Form.Control.Feedback type="invalid">
-              {errors.confirmPassword}
-            </Form.Control.Feedback>
-          </Form.Group>
-        </Col>
-      </Row>
-      <Row className="longInput">
-        <Col md={6}>
-          <Form.Group className="mb-3">
-            <Form.Label>Email</Form.Label>
-            <Form.Control
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleInputChange}
-              autoComplete="off"
-              required
-            />
-            <Form.Control.Feedback type="invalid">
-              {errors.email}
-            </Form.Control.Feedback>
-          </Form.Group>
-        </Col>
-      </Row>
-      <Row>
-        <Col md={6}>
-          <Form.Group className="mb-3">
-            <Form.Label>First Name</Form.Label>
-            <Form.Control
-              type="text"
-              name="firstName"
-              value={formData.firstName}
-              onChange={handleInputChange}
-              autoComplete="off"
-              required
-            />
-            <Form.Control.Feedback type="invalid">
-              {errors.firstName}
-            </Form.Control.Feedback>
-          </Form.Group>
-        </Col>
-        <Col md={6}>
-          <Form.Group className="mb-3">
-            <Form.Label>Last Name</Form.Label>
-            <Form.Control
-              type="text"
-              name="lastName"
-              value={formData.lastName}
-              onChange={handleInputChange}
-              autoComplete="off"
-              required
-            />
-            <Form.Control.Feedback type="invalid">
-              {errors.lastName}
-            </Form.Control.Feedback>
-          </Form.Group>
-        </Col>
-      </Row>
+  return (
+  <div className="signUpImg">
 
-      <Row className="longInput">
-        <Col md={6}>
-          <Form.Group className="mb-3">
-            <Form.Label>Address</Form.Label>
-            <Form.Control
-              type="text"
-              name="address"
-              value={formData.address}
-              onChange={handleInputChange}
-              autoComplete="off"
-              required
-            />
-            <Form.Control.Feedback type="invalid">
-              {errors.address}
-            </Form.Control.Feedback>
-          </Form.Group>
-        </Col>
-      </Row>
-      <Row className="longInput">
-        <Col md={6}>
-          <Form.Group className="mb-3">
-            <Form.Label>City</Form.Label>
-            <Form.Control
-              type="text"
-              name="city"
-              value={formData.city}
-              onChange={handleInputChange}
-              autoComplete="off"
-              required
-            />
-            <Form.Control.Feedback type="invalid">
-              {errors.city}
-            </Form.Control.Feedback>
-          </Form.Group>
-        </Col>
-      </Row>
-      <Row>
-        <Col md={6}>
-          <Form.Group className="mb-3">
-            <Form.Label>State</Form.Label>
-            <Form.Control
-              type="text"
-              name="state"
-              value={formData.state}
-              onChange={handleInputChange}
-              autoComplete="off"
-              required
-            />
-            <Form.Control.Feedback type="invalid">
-              {errors.state}
-            </Form.Control.Feedback>
-          </Form.Group>
-        </Col>
-        <Col md={6}>
-          <Form.Group className="mb-3">
-            <Form.Label>Zip</Form.Label>
-            <Form.Control
-              type="text"
-              name="zipCode"
-              value={formData.zipCode}
-              onChange={handleInputChange}
-              autoComplete="off"
-              required
-            />
-            <Form.Control.Feedback type="invalid">
-              {errors.zipCode}
-            </Form.Control.Feedback>
-          </Form.Group>
-        </Col>
-      </Row>
-      <Row className="longInput">
-        <Col md={6}>
-          <Form.Group className="mb-3">
-            <Form.Label>Favorite Genre</Form.Label>
-            <Form.Control
-              type="text"
-              name="favGenres"
-              value={formData.favGenres}
-              onChange={handleInputChange}
-              autoComplete="off"
-            />
-            <Form.Control.Feedback type="invalid">
-              {errors.favGenres}
-            </Form.Control.Feedback>
-          </Form.Group>
-        </Col>
-      </Row>
-      <Row className="mb-3">
-        <Form.Label>Select your Avatar:</Form.Label>
-        <div className="avatarContainer">
-          {avatarsList.map((avatar, index) => (
-            <div className={`avatar-select ${formData.selectedAvatar === avatar ? 'avatar-selected' : ''}`}
-             key={index} 
-             onClick={() => handleAvatarSelection(avatar)}>
-              <img src={avatar} alt={`Avatar ${index}`} />
+    <Container >
+      <h1>Create an Account</h1>
+      <Form className="auth-form" onSubmit={handleSubmit}>
+        <Row className="longInput">
+          <Col md={6}>
+            <Form.Group className="mb-3">
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                type="text"
+                name="userName"
+                value={formData.userName}
+                onChange={handleInputChange}
+                autoComplete="off"
+                required
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.userName}
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={6}>
+            <Form.Group className="mb-3">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleInputChange}
+                autoComplete="off"
+                required
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.password}
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Col>
+          <Col md={6}>
+            <Form.Group className="mb-3">
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control
+                type="password"
+                name="confirmPassword"
+                value={formData.confirmPassword}
+                onChange={handleInputChange}
+                required
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.confirmPassword}
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row className="longInput">
+          <Col md={6}>
+            <Form.Group className="mb-3">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleInputChange}
+                autoComplete="off"
+                required
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.email}
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={6}>
+            <Form.Group className="mb-3">
+              <Form.Label>First Name</Form.Label>
+              <Form.Control
+                type="text"
+                name="firstName"
+                value={formData.firstName}
+                onChange={handleInputChange}
+                autoComplete="off"
+                required
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.firstName}
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Col>
+          <Col md={6}>
+            <Form.Group className="mb-3">
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control
+                type="text"
+                name="lastName"
+                value={formData.lastName}
+                onChange={handleInputChange}
+                autoComplete="off"
+                required
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.lastName}
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Col>
+        </Row>
+
+        <Row className="longInput">
+          <Col md={6}>
+            <Form.Group className="mb-3">
+              <Form.Label>Address</Form.Label>
+              <Form.Control
+                type="text"
+                name="address"
+                value={formData.address}
+                onChange={handleInputChange}
+                autoComplete="off"
+                required
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.address}
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row className="longInput">
+          <Col md={6}>
+            <Form.Group className="mb-3">
+              <Form.Label>City</Form.Label>
+              <Form.Control
+                type="text"
+                name="city"
+                value={formData.city}
+                onChange={handleInputChange}
+                autoComplete="off"
+                required
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.city}
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={6}>
+            <Form.Group className="mb-3">
+              <Form.Label>State</Form.Label>
+              <Form.Control
+                type="text"
+                name="state"
+                value={formData.state}
+                onChange={handleInputChange}
+                autoComplete="off"
+                required
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.state}
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Col>
+          <Col md={6}>
+            <Form.Group className="mb-3">
+              <Form.Label>Zip</Form.Label>
+              <Form.Control
+                type="text"
+                name="zipCode"
+                value={formData.zipCode}
+                onChange={handleInputChange}
+                autoComplete="off"
+                required
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.zipCode}
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row className="longInput">
+          <Col md={6}>
+            <Form.Group className="mb-3">
+              <Form.Label>Favorite Genre</Form.Label>
+              <Form.Control
+                type="text"
+                name="favGenres"
+                value={formData.favGenres}
+                onChange={handleInputChange}
+                autoComplete="off"
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.favGenres}
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row className="mb-3">
+          <Form.Label>Select your Avatar:</Form.Label>
+          <div className="avatarContainer">
+            {avatarsList.map((avatar, index) => (
+              <div className={`avatar-select ${formData.selectedAvatar === avatar ? 'avatar-selected' : ''}`}
+                key={index}
+                onClick={() => handleAvatarSelection(avatar)}>
+                <img src={avatar} alt={`Avatar ${index}`} />
               </div>
-          ))}
-        </div>
-      </Row>
-      <Form.Group className="mb-3">
-        <Button type="submit">Create Account</Button>
-      </Form.Group>
-      <Form.Text>
-        Already Signed Up? <Link to="/login">Login</Link>
-      </Form.Text>
-    </Form>
-  </Container>
+            ))}
+          </div>
+        </Row>
+        <Form.Group className="mb-3">
+          <Button type="submit">Create Account</Button>
+        </Form.Group>
+        <Form.Text>
+          Already Signed Up? <Link to="/login">Login</Link>
+        </Form.Text>
+      </Form>
+    </Container>
+  </div>
+  )
 }
 
 export default SignUpPage;
